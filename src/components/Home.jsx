b4a58467fe3aa5b78c9937cf06b2939aef8f8496
Brandon <brandoncastillo.09@gmail.com>
2024-07-10 16:28:27 -0300

    home desktop ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,21 +12,44 @@ function Home() {
 
   return (
     <section className="home-compo" id="home">
-      <figure>
-        <img src={image} alt="home-image" />
-      </figure>
+      <div className="home-mobile">
+        <figure>
+          <img src={image} alt="home-image" />
+        </figure>
 
-      <h1> Calle'$ Construcction</h1>
+        <h1> Calle'$ Construcction</h1>
 
-      <p>
-        We offer a variety of professional contracting services to meet all your
-        needs.
-      </p>
+        <p>
+          We offer a variety of professional contracting services to meet all
+          your needs.
+        </p>
 
-      <div className="button-estimate-div">
-        <button onClick={handleGo} className="button">
-          Get Estimate
-        </button>
+        <div className="button-estimate-div">
+          <button onClick={handleGo} className="button">
+            Get Estimate
+          </button>
+        </div>
+      </div>
+
+      <div className="home-desktop">
+        <div className="home-desktop-title">
+          <h1>
+            Calle'$ <br />
+            Construcction
+          </h1>
+          <p>
+            We offer a variety of professional contracting services to meet all
+            your needs.
+          </p>
+          <div className="button-estimate-div">
+            <button onClick={handleGo} className="button">
+              Get Estimate
+            </button>
+          </div>
+        </div>
+        <figure>
+          <img src={image} alt="home-image" />
+        </figure>
       </div>
     </section>
   );
