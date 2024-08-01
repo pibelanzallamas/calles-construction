5db50804431f8901cd3ef9609f84463a991c9b7f
Brandon <brandoncastillo.09@gmail.com>
2024-08-01 08:58:44 -0300

    footer mobile optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import "./app.css";
 import { useState } from "react";
-import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Hidden from "./components/Hidden";
-import Services from "./components/Services";
-import Jobs from "./components/Jobs";
-import Gallery from "./components/Gallery";
-import Estimate from "./components/Estimate";
-import Location from "./components/Location";
 import Footer from "./components/Footer";
 import AppRoutes from "./components/AppRoutes";
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const [hamburger, setHamburger] = useState(false);
 
   function handleOpen(state) {
-    setOpen(state);
+    setHamburger(state);
   }
 
   return (
     <>
       <Navbar openFunc={handleOpen} />
-      <Hidden isOpen={open} />
+      <Hidden isOpen={hamburger} />
       <div className="home">
         <AppRoutes />
-        {/* <Home /> */}
-        {/* <Services /> */}
-        {/* <Jobs /> */}
-        {/* <Gallery /> */}
-        {/* <Estimate /> */}
-        {/* <Location /> */}
       </div>
       <Footer />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
