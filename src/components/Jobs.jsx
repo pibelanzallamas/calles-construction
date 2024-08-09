4ac0fda43912753578ed6b638fe90e55527d5c81
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-08-08 21:33:21 -0300

    more/less job button

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -5,6 +5,8 @@ import { useSelector } from "react-redux";
 import { services } from "../utilities/services";
 import jobs from "../assets/jobs-img.jpg";
 import TopButton from "../commons/TopButton";
+import moreButton from "../assets/moreButton.svg";
+import lessButton from "../assets/lessButton.svg";
 
 function Jobs() {
   const texts = [
@@ -18,58 +20,105 @@ function Jobs() {
     },
   ];
   const user = useSelector((state) => state.user);
+
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [desc, setDesc] = useState("");
   const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [more, setMore] = useState(false);
+
+  function handleMore() {
+    setMore(true);
+  }
+
+  function handleLess() {
+    setMore(false);
+  }
+
+  function createJobs() {
+    setLoading(true);
+  }
 
   return (
     <section id="jobs" className="jobs-compo">
       <h2>Jobs</h2>
 
-      <form onSubmit={handleLogin}>
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-            maxLength={40}
-            placeholder="email"
-          />
-        </div>
-        <div className="field">
-          <div className="peak-line">
-            <label>Password</label>
-            <figure onClick={handlePeak}>
-              <img src={peak ? eyeOpen : eyeClose}></img>
+      {user.id && (
+        <>
+          {more ? (
+            <figure onClick={handleLess} className="more-button">
+              <img src={lessButton} alt="less-button"></img>
             </figure>
-          </div>
-          <input
-            type={peak ? "text" : "password"}
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-            maxLength={20}
-            placeholder="password"
-          />
-        </div>
-        {loading ? (
-          <p className="loading-text"> Loading ...</p>
-        ) : (
-          <button type="submit">Send</button>
-        )}
-      </form>
-
+          ) : (
+            <figure onClick={handleMore} className="more-button">
+              <img src={moreButton} alt="more-button"></img>
+            </figure>
+          )}
+          {more && (
+            <div className="estimate-compo form-job">
+              <form onSubmit={createJobs}>
+                <div className="field">
+                  <label htmlFor="title">Title</label>
+                  <input
+                    id="title"
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    required
+                    maxLength={25}
+                    placeholder="title"
+                  />
+                </div>
+                <div className="field">
+                  <label>Date</label>
+                  <input
+                    type="date"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                    required
+                    maxLength={20}
+                    placeholder="date"
+                  />
+                </div>
+                <div className="field">
+                  <label>Description</label>
+                  <textarea
+                    type="text"
+                    onChange={(e) => setDesc(e.target.value)}
+                    value={desc}
+                    required
+                    rows={5}
+                    maxLength={140}
+                    placeholder="description"
+                  />
+                </div>
+                <div className="field">
+                  <label>Image</label>
+                  <input
+                    type="text"
+                    onChange={(e) => setImage(e.target.value)}
+                    value={image}
+                    required
+                    maxLength={20}
+                    placeholder="image"
+                  />
+                </div>
+                {loading ? (
+                  <p className="loading-text"> Loading ...</p>
+                ) : (
+                  <button type="submit">Send</button>
+                )}
+              </form>
+            </div>
+          )}
+        </>
+      )}
       {/* <figure className="jobs-img">
         <img src={jobs} alt="jobs-img" />
-      </figure> */}
+      </figure>
 
-      {/* {jobs.length > 0 && jobs.map(())} */}
-
-      {/* <Text text={texts[0]} />
+      <Text text={texts[0]} />
 
       <Job service={services[0]} />
 
@@ -84,7 +133,6 @@ function Jobs() {
       <Job service={services[4]} />
 
       <Job service={services[5]} /> */}
-
       <TopButton />
     </section>
   );
