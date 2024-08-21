52ed38213f5c5132aa5df930eab94779d6a666e8
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-08-21 12:58:44 -0300

    rsz btn change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,53 @@ import { Link } from "react-router-dom";
 function Navbar({ openFunc }) {
   const [isOpen, setOpen] = useState(false);
   const user = useSelector((state) => state.user);
+  const [nl, setNl] = useState(""); //link
+  const [i, setI] = useState(""); //imagen
+  const [finalLogo, setFinalLogo] = useState("");
 
   useEffect(() => {
     openFunc(isOpen);
   }, [isOpen]);
+
+  function subirImagen() {
+    //poner url en nl
+  }
+
+  function handleLogo() {
+    //seleccionar un logo .svg, .png, .jpeg
+    //subir a cloudinary, en carpeta logos
+    //obtener url www.site.com/archivo.jpg
+    //cambiar el src de img
+    //se muestra la imagen
+    setNl(
+      "https://upload.wikimedia.org/wikipedia/commons/4/4a/100x100_logo.png"
+    );
+  }
+
+  useEffect(() => {
+    if (nl) setFinalLogo(nl);
+    else setFinalLogo(logo);
+  }, [nl]);
+
+  /*
+   * buscar remotamente y obtener el ultimo
+   * si el usuario carga uno, subirlo
+   * obtener el ultimo de nuevo y recargar el valor
+   */
 
   return (
     <nav id="navbar">
       <div className="logo-section">
         <Link to="/">
           <figure className="nav-logo">
-            <img src={logo} alt="calles-logo" />
+            <img src={finalLogo} alt="calles-logo" />
           </figure>
         </Link>
-        {user.id && <button>Change</button>}
+        {user.id && (
+          <button id="logo-button" onClick={handleLogo}>
+            Change
+          </button>
+        )}
       </div>
       <ul className="desktop-navbar">
         <Link to="/services">Services</Link>
