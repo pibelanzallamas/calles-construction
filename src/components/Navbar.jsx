fa62537b5319961ac8ce8142feefb3b27161d2c7
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-08-21 10:05:28 -0300

    change button for logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/nav-logo.png";
 import { Squash as Hamburger } from "hamburger-react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Navbar({ openFunc }) {
   const [isOpen, setOpen] = useState(false);
+  const user = useSelector((state) => state.user);
 
   useEffect(() => {
     openFunc(isOpen);
@@ -12,11 +14,14 @@ function Navbar({ openFunc }) {
 
   return (
     <nav id="navbar">
-      <Link to="/">
-        <figure className="nav-logo">
-          <img src={logo} alt="calles-logo" />
-        </figure>
-      </Link>
+      <div className="logo-section">
+        <Link to="/">
+          <figure className="nav-logo">
+            <img src={logo} alt="calles-logo" />
+          </figure>
+        </Link>
+        {user.id && <button>Change</button>}
+      </div>
       <ul className="desktop-navbar">
         <Link to="/services">Services</Link>
         <Link to="/jobs">Jobs</Link>
--- src/state/userState.js
+++ src/state/userState.js
@@ -3,8 +3,8 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 export const setUser = createAction("SET_USER");
 
 const initialState = {
-  id: null,
-  email: null,
+  id: 1,
+  email: "admin@email.com",
 };
 
 const userReducer = createReducer(initialState, (builder) => {
